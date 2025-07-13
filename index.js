ef62eb2715dd8273c755b4f1d5c91367b67fde44
Coulibaly Ibrahima <50629646+ibra-cadabra@users.noreply.github.com>
2025-07-13 19:31:39 +0200

    ajout de app.use(cors)

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ import alertRoutes from "./routes/alert.routes.js";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+
+app.use(cors({
+  origin: ['https://fxn-frontend-imhb.vercel.app'], // ← ton frontend
+  credentials: true
+}));
+
 app.use(express.json());
 app.use('/alerts', alertRoutes); // 💡 accessible sur /alerts/consumables
 app.use('/auth', authRoutes);
